4d1482f2f3fbd99e35a18d34ac4a8822fec38944
Pistolmannen <erikgustafsson@gmail.com>
2024-04-22 14:24:38 +0200

    made som mountens and started to give recycler view functionalety

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -11,10 +16,22 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
+    private RecyclerView resView;
+    private RecyclerViewAdapter resViewAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList Mountains = new ArrayList<>(Arrays.asList(
+            new Mountain("Billigen"),
+            new Mountain("Mount Everest"),
+            new Mountain("Uluru")
+        ));
+
+        resView = findViewById(R.id.recycler_view);
+        resView.setLayoutManager(new LinearLayoutManager(this));
 
         new JsonFile(this, this).execute(JSON_FILE);
     }
